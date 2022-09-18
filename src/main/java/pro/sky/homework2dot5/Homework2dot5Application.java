26980ddb46c60799994304380acc51911e005d12
kirillstrizenko <krealstrzh@icloud.com>
2022-09-18 15:03:34 +0300

    HW2_5

---
+++ src/main/java/pro/sky/homework2dot5/Homework2dot5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework2dot5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework2dot5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework2dot5Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/homework2dot5/Homework2dot5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework2dot5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework2dot5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
